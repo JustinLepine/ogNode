8806ae180a1e3f4b8743241229b654a6f5610ec7
Justin Lepine <justin.lepine@plusgrade.com>
2023-02-04 10:24:29 -0500

    post now working dynamically

--- models/productModel.js
+++ models/productModel.js
@@ -1,17 +1,17 @@
 const products = require('../data/products');
-const { writeDataToFile } = require('../utils')
+const { writeDataToFile } = require('../utils');
 const { v4: uuidv4 } = require('uuid');
 
 function findAll() {
   return new Promise((resolve, reject) => {
-    resolve(products)
+    resolve(products);
   })
 }
 
 function findById(id) {
   return new Promise((resolve, reject) => {
-    const product = products.find((product) => product.id === id)
-    resolve(product)
+    const product = products.find((product) => product.id === id);
+    resolve(product);
   })
 }
 
@@ -20,7 +20,7 @@ function create(product) {
     const newProduct = {id: uuidv4(), ...product}
     products.push(newProduct);
     writeDataToFile('./data/products.json', products);
-    resolve(newProduct)
+    resolve(newProduct);
   })
 }
 
--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function writeDataToFile(fileName, content) {
   fs.writeFileSync(fileName, JSON.stringify(content), 'utf8', (err) => {
     if(err) {
-      console.log(err)
+      console.log(err);
     }
   })
 }
